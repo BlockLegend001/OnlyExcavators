13ea6f9ba95dbbfc06f9302ac1c404a472352f3c
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:19:10 +0100

    Aggiunta 1.19.2-Forge

---
+++ src/main/java/com/blocklegend001/onlyexcavators/datagen/DataGenerators.java
@@ -0,0 +1,19 @@
+package com.blocklegend001.onlyexcavators.datagen;
+
+import com.blocklegend001.onlyexcavators.OnlyExcavators;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = OnlyExcavators.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        generator.addProvider(true, new ModItemModelProvider(generator, existingFileHelper));
+    }
+}
