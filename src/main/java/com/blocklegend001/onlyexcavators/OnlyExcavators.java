150742f42afc1f1bdc23c2b7d5d9240ea52cb176
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:20:26 +0100

    Aggiunta 1.19.4-Fabric

---
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
@@ -0,0 +1,18 @@
+package com.blocklegend001.onlyexcavators;
+
+import com.blocklegend001.onlyexcavators.item.ModItemGroup;
+import com.blocklegend001.onlyexcavators.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OnlyExcavators implements ModInitializer {
+    public static final String MOD_ID = "onlyexcavators";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ModItemGroup.registerItemGroups();
+        ModItems.registerModItems();
+    }
+}
