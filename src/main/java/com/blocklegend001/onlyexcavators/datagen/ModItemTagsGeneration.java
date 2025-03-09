1235ec1fe67563ba34c2535b8b5e7dbda04bee78
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:20:51 +0100

    Aggiunta 1.21-Fabric

---
+++ src/main/java/com/blocklegend001/onlyexcavators/datagen/ModItemTagsGeneration.java
@@ -0,0 +1,74 @@
+package com.blocklegend001.onlyexcavators.datagen;
+
+import com.blocklegend001.onlyexcavators.item.ModItems;
+import com.blocklegend001.onlyexcavators.utils.ModTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagsGeneration extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagsGeneration(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(dataOutput, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ItemTags.DURABILITY_ENCHANTABLE).add(ModItems.WOODEN_EXCAVATOR)
+                .add(ModItems.STONE_EXCAVATOR)
+                .add(ModItems.IRON_EXCAVATOR)
+                .add(ModItems.GOLD_EXCAVATOR)
+                .add(ModItems.LAPIS_EXCAVATOR)
+                .add(ModItems.REDSTONE_EXCAVATOR)
+                .add(ModItems.OBSIDIAN_EXCAVATOR)
+                .add(ModItems.DIAMOND_EXCAVATOR)
+                .add(ModItems.EMERALD_EXCAVATOR)
+                .add(ModItems.NETHERITE_EXCAVATOR);
+
+        getOrCreateTagBuilder(ItemTags.MINING_LOOT_ENCHANTABLE).add(ModItems.WOODEN_EXCAVATOR)
+                .add(ModItems.STONE_EXCAVATOR)
+                .add(ModItems.IRON_EXCAVATOR)
+                .add(ModItems.GOLD_EXCAVATOR)
+                .add(ModItems.LAPIS_EXCAVATOR)
+                .add(ModItems.REDSTONE_EXCAVATOR)
+                .add(ModItems.OBSIDIAN_EXCAVATOR)
+                .add(ModItems.DIAMOND_EXCAVATOR)
+                .add(ModItems.EMERALD_EXCAVATOR)
+                .add(ModItems.NETHERITE_EXCAVATOR);
+
+        getOrCreateTagBuilder(ItemTags.VANISHING_ENCHANTABLE).add(ModItems.WOODEN_EXCAVATOR)
+                .add(ModItems.STONE_EXCAVATOR)
+                .add(ModItems.IRON_EXCAVATOR)
+                .add(ModItems.GOLD_EXCAVATOR)
+                .add(ModItems.LAPIS_EXCAVATOR)
+                .add(ModItems.REDSTONE_EXCAVATOR)
+                .add(ModItems.OBSIDIAN_EXCAVATOR)
+                .add(ModItems.DIAMOND_EXCAVATOR)
+                .add(ModItems.EMERALD_EXCAVATOR)
+                .add(ModItems.NETHERITE_EXCAVATOR);
+
+        getOrCreateTagBuilder(ItemTags.MINING_ENCHANTABLE).add(ModItems.WOODEN_EXCAVATOR)
+                .add(ModItems.STONE_EXCAVATOR)
+                .add(ModItems.IRON_EXCAVATOR)
+                .add(ModItems.GOLD_EXCAVATOR)
+                .add(ModItems.LAPIS_EXCAVATOR)
+                .add(ModItems.REDSTONE_EXCAVATOR)
+                .add(ModItems.OBSIDIAN_EXCAVATOR)
+                .add(ModItems.DIAMOND_EXCAVATOR)
+                .add(ModItems.EMERALD_EXCAVATOR)
+                .add(ModItems.NETHERITE_EXCAVATOR);
+
+        getOrCreateTagBuilder(ModTags.Items.WOODEN_EXCAVATOR).add(ModItems.WOODEN_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.STONE_EXCAVATOR).add(ModItems.STONE_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.IRON_EXCAVATOR).add(ModItems.IRON_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.GOLD_EXCAVATOR).add(ModItems.GOLD_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.LAPIS_EXCAVATOR).add(ModItems.LAPIS_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.REDSTONE_EXCAVATOR).add(ModItems.REDSTONE_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.OBSIDIAN_EXCAVATOR).add(ModItems.OBSIDIAN_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.DIAMOND_EXCAVATOR).add(ModItems.DIAMOND_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.EMERALD_EXCAVATOR).add(ModItems.EMERALD_EXCAVATOR);
+        getOrCreateTagBuilder(ModTags.Items.NETHERITE_EXCAVATOR).add(ModItems.NETHERITE_EXCAVATOR);
+    }
+}
---
+++ src/main/java/com/blocklegend001/onlyexcavators/item/custom/EmeraldExcavator.java
@@ -0,0 +1,18 @@
+package com.blocklegend001.onlyexcavators.item.custom;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+
+public class EmeraldExcavator extends Excavator {
+
+    public EmeraldExcavator(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean canRepair(ItemStack stack, ItemStack ingredient) {
+        return ingredient.getItem() == Items.EMERALD;
+    }
+}
+
