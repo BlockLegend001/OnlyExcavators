7fc6d4f122b46c186ff4ac4a5dda3e702bc2a5bb
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-15 17:12:52 +0100

    Added 1.21.5-Fabric

---
+++ src/main/java/com/blocklegend001/onlyexcavators/client/OnlyExcavatorsClient.java
@@ -0,0 +1,10 @@
+package com.blocklegend001.onlyexcavators.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OnlyExcavatorsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
