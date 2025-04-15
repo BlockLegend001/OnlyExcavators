6461607e7032c72546c739ce7026dca88c2f7889
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-15 17:13:26 +0100

    Added 1.21.5-NeoForge

---
+++ src/main/java/com/blocklegend001/onlyexcavators/ModEvents.java
@@ -0,0 +1,40 @@
+package com.blocklegend001.onlyexcavators;
+
+import com.blocklegend001.onlyexcavators.item.custom.Excavator;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.level.BlockEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@EventBusSubscriber(modid = OnlyExcavators.MOD_ID)
+public class ModEvents {
+    private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    @SubscribeEvent
+    public static void onHammerUsage(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        ItemStack mainHandItem = player.getMainHandItem();
+
+        if(mainHandItem.getItem() instanceof Excavator excavator && player instanceof ServerPlayer serverPlayer) {
+            BlockPos initalBlockPos = event.getPos();
+            if (HARVESTED_BLOCKS.contains(initalBlockPos)) {
+                return;
+            }
+
+            for (BlockPos pos : Excavator.getBlocksToBeDestroyed(1, initalBlockPos, serverPlayer)) {
+                if(pos == initalBlockPos || !excavator.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
+                    continue;
+                }
+
+                HARVESTED_BLOCKS.add(pos);
+                serverPlayer.gameMode.destroyBlock(pos);
+                HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
@@ -0,0 +1,49 @@
+package com.blocklegend001.onlyexcavators;
+
+import com.blocklegend001.onlyexcavators.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
+
+@Mod(OnlyExcavators.MOD_ID)
+public class OnlyExcavators {
+
+    public static final String MOD_ID = "onlyexcavators";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ONLYEXCAVATORS = CREATIVE_MODE_TABS.register("onlyexcavators", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.onlyexcavators.onlyexcavators"))
+            .icon(() -> new ItemStack(ModItems.NETHERITE_EXCAVATOR.get()))
+            .displayItems((parameters, output) -> {
+                output.accept(ModItems.WOODEN_EXCAVATOR.get());
+                output.accept(ModItems.STONE_EXCAVATOR.get());
+                output.accept(ModItems.IRON_EXCAVATOR.get());
+                output.accept(ModItems.GOLD_EXCAVATOR.get());
+                output.accept(ModItems.LAPIS_EXCAVATOR.get());
+                output.accept(ModItems.REDSTONE_EXCAVATOR.get());
+                output.accept(ModItems.DIAMOND_EXCAVATOR.get());
+                output.accept(ModItems.EMERALD_EXCAVATOR.get());
+                output.accept(ModItems.OBSIDIAN_EXCAVATOR.get());
+                output.accept(ModItems.NETHERITE_EXCAVATOR.get());
+            }).build()
+    );
+
+    public OnlyExcavators(IEventBus eventBus, ModContainer container) {
+        ModConfigs.loadConfig();
+        NeoForge.EVENT_BUS.register(ModEvents.class);
+        ModItems.register(eventBus);
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
