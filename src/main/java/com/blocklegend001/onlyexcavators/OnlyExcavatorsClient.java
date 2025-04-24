aecaed7d1898ff34bf7c04885b24dac1c71f0a4d
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-24 11:51:42 +0200

    Added Highlights Outline to the Blocks

--- src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavatorsClient.java
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavatorsClient.java
@@ -1,10 +1,11 @@
-package com.blocklegend001.onlyexcavators.client;
+package com.blocklegend001.onlyexcavators;
 
+import com.blocklegend001.onlyexcavators.utils.ExcavatorOverlayRenderer;
 import net.fabricmc.api.ClientModInitializer;
 
 public class OnlyExcavatorsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ExcavatorOverlayRenderer.init();
     }
 }
