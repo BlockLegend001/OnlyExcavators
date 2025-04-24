66325731a8b1920153942e205d741f30e3858670
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-24 12:03:50 +0200

    Added Highlights Outline to the Blocks

--- src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
@@ -1,6 +1,7 @@
 package com.blocklegend001.onlyexcavators;
 
 import com.blocklegend001.onlyexcavators.item.ModItems;
+import com.blocklegend001.onlyexcavators.utils.ExcavatorOverlayRenderer;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -44,6 +45,7 @@ public class OnlyExcavators {
         ModConfigs.loadConfig();
         NeoForge.EVENT_BUS.register(ModEvents.class);
         ModItems.register(eventBus);
+        NeoForge.EVENT_BUS.register(ExcavatorOverlayRenderer.class);
         CREATIVE_MODE_TABS.register(eventBus);
     }
 }
