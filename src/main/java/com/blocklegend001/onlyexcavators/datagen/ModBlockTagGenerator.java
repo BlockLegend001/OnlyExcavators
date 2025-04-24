07ec215a196c90118a43b0187819c66ad65c7efa
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-24 12:07:24 +0200

    Some fixes and more

--- src/main/java/com/blocklegend001/onlyexcavators/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/blocklegend001/onlyexcavators/datagen/ModBlockTagGenerator.java
@@ -4,14 +4,13 @@ import com.blocklegend001.onlyexcavators.OnlyExcavators;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagGenerator extends BlockTagsProvider {
-    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
-        super(output, lookupProvider, OnlyExcavators.MOD_ID, existingFileHelper);
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(output, lookupProvider, OnlyExcavators.MOD_ID);
     }
 
     @Override
