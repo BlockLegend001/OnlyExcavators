0bc38cf544c0fe9bcba71b83a2bb87a58805aaef
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-06 18:27:04 +0200

    Fixed a bug that caused the server to crash and other changes

--- src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
@@ -7,7 +7,6 @@ import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
