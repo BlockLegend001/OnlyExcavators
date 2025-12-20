f81dd612ec3e77b695e334bfbf3401d883c58d25
BlockLegend001 <rosarioarcamone@gmail.com>
2025-12-20 14:16:29 +0100

    Added 1.21.11-NeoForge

---
+++ src/main/java/com/blocklegend001/onlyexcavators/datagen/ModItemModelProvider.java
@@ -0,0 +1,50 @@
+package com.blocklegend001.onlyexcavators.datagen;
+
+import com.blocklegend001.onlyexcavators.item.ModItems;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ModelTemplate;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.client.renderer.item.BlockModelWrapper;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.Item;
+
+import java.util.Collections;
+
+public class ModItemModelProvider extends ModelProvider {
+    public ModItemModelProvider(PackOutput output, String modID) {
+        super(output, modID);
+    }
+
+    @Override
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        itemModel(itemModels, ModItems.WOODEN_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.STONE_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.IRON_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.GOLD_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.REDSTONE_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.LAPIS_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.DIAMOND_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.EMERALD_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.OBSIDIAN_EXCAVATOR.get());
+        itemModel(itemModels, ModItems.NETHERITE_EXCAVATOR.get());
+    }
+
+    public void itemModel(ItemModelGenerators itemModels, Item item)
+    {
+        this.itemModel(itemModels, item, ModelTemplates.FLAT_ITEM);
+    }
+
+    public void itemModel(ItemModelGenerators itemModels, Item item, ModelTemplate template)
+    {
+        Identifier itemId = BuiltInRegistries.ITEM.getKey(item);
+        Identifier textureLoc = Identifier.fromNamespaceAndPath(itemId.getNamespace(), "item/" + itemId.getPath());
+        TextureMapping textureMapping = new TextureMapping().put(TextureSlot.LAYER0, textureLoc);
+        itemModels.itemModelOutput.accept(item, new BlockModelWrapper.Unbaked(template.create(item, textureMapping, itemModels.modelOutput), Collections.emptyList()));
+    }
+}
