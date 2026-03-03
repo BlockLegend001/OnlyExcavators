1973ec4ab61b61facfa61884d6bcf999bdad8838
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-03 15:15:33 +0100

    Added KeyBind to Enable/Disable the Outline

---
+++ src/main/java/com/blocklegend001/onlyexcavators/utils/KeyBinding.java
@@ -0,0 +1,16 @@
+package com.blocklegend001.onlyexcavators.utils;
+
+import com.blocklegend001.onlyexcavators.OnlyExcavators;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = OnlyExcavators.MOD_ID, value = Dist.CLIENT)
+public class KeyBinding {
+
+    @SubscribeEvent
+    public static void registerKeys(RegisterKeyMappingsEvent event) {
+        event.register(OnlyExcavators.SHOW_OUTLINE_KEY.get());
+    }
+}
