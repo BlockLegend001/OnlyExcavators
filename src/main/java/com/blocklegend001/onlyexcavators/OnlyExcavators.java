8a2fa2406fba085cc45d7f5c0df46892a9de5625
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-03 18:04:46 +0100

    Added KeyBind to Enable/Disable the Outline

--- src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
@@ -2,17 +2,22 @@ package com.blocklegend001.onlyexcavators;
 
 import com.blocklegend001.onlyexcavators.event.ModEvents;
 import com.blocklegend001.onlyexcavators.item.ModItems;
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.common.util.Lazy;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
 @Mod(OnlyExcavators.MOD_ID)
@@ -20,6 +25,22 @@ public class OnlyExcavators {
 
     public static final String MOD_ID = "onlyexcavators";
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static boolean SHOW_OUTLINE_ENABLED = true;
+
+    public static final KeyMapping.Category ONLYEXCAVATORS_CATEGORY =
+            KeyMapping.Category.register(
+                    ResourceLocation.fromNamespaceAndPath(MOD_ID, "showoutline")
+            );
+
+    public static final Lazy<KeyMapping> SHOW_OUTLINE_KEY = Lazy.of(() ->
+            new KeyMapping(
+                    "key.onlyexcavators.showoutline",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_O,
+                    ONLYEXCAVATORS_CATEGORY
+            )
+    );
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
 
