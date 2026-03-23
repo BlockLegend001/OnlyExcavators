0e061e26eb49e71bb911b22535e4827de354305c
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-23 12:37:45 +0100

    Fixed a bug that let the server crash with the latest update

--- src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
+++ src/main/java/com/blocklegend001/onlyexcavators/OnlyExcavators.java
@@ -2,18 +2,13 @@ package com.blocklegend001.onlyexcavators;
 
 import com.blocklegend001.onlyexcavators.item.ModCreativeModeTabs;
 import com.blocklegend001.onlyexcavators.item.ModItems;
-import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.KeyMapping;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
 @Mod(OnlyExcavators.MOD_ID)
@@ -22,15 +17,6 @@ public class OnlyExcavators {
     public static final String MOD_ID = "onlyexcavators";
     public static final Logger LOGGER = LogUtils.getLogger();
     public static boolean SHOW_OUTLINE_ENABLED = true;
-
-    public static final Lazy<KeyMapping> SHOW_OUTLINE_KEY = Lazy.of(() ->
-            new KeyMapping(
-                    "key.onlyexcavators.showoutline",
-                    InputConstants.Type.KEYSYM,
-                    GLFW.GLFW_KEY_O,
-                    "key.category.onlyexcavators.showoutline"
-            )
-    );
     
     public OnlyExcavators(FMLJavaModLoadingContext context) {
         var modGroupBus = context.getModBusGroup();
