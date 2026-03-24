8d4cced58f4235905ad4ea63da3a37701acf18a9
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-24 19:36:44 +0100

    Fixed a bug that let the server crash with the latest update

--- src/main/java/com/blocklegend001/onlyexcavators/utils/KeyBinding.java
+++ src/main/java/com/blocklegend001/onlyexcavators/utils/KeyBinding.java
@@ -1,15 +1,28 @@
 package com.blocklegend001.onlyexcavators.utils;
 
 import com.blocklegend001.onlyexcavators.OnlyExcavators;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.common.util.Lazy;
+import org.lwjgl.glfw.GLFW;
 
 @EventBusSubscriber(modid = OnlyExcavators.MOD_ID, value = Dist.CLIENT)
 public class KeyBinding {
+    public static final Lazy<KeyMapping> SHOW_OUTLINE_KEY = Lazy.of(() ->
+            new KeyMapping(
+                    "key.onlyexcavators.showoutline",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_O,
+                    "key.category.onlyexcavators.showoutline"
+            )
+    );
+
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event) {
-        event.register(OnlyExcavators.SHOW_OUTLINE_KEY.get());
+        event.register(SHOW_OUTLINE_KEY.get());
     }
 }
